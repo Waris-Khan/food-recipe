0660593fad970b3e955b1dbb54a86fd3ebfeaf50
Waris-Khan <k.waris14@gmail.com>
2020-08-24 11:35:05 +0500

    Updates

---
+++ precache-manifest.f883b062f0c9cd87cc6dafaf063de7f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742f6979f300bbb4c9425d57345fa678",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "86a29cd1809596032f87",
+    "url": "/food-recipe/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "57319021947f941a123b",
+    "url": "/food-recipe/static/css/main.90648b3c.chunk.css"
+  },
+  {
+    "revision": "86a29cd1809596032f87",
+    "url": "/food-recipe/static/js/2.c29720f9.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/food-recipe/static/js/2.c29720f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57319021947f941a123b",
+    "url": "/food-recipe/static/js/main.b572378c.chunk.js"
+  },
+  {
+    "revision": "779335723206f8e70329",
+    "url": "/food-recipe/static/js/runtime-main.3dc38f30.js"
+  }
+]);
